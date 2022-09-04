9abbd49d57b3935148e106acc58c798638b03a99
2480258 <swh8059@naver.com>
2022-09-04 16:14:59 +0900

    added detail url

--- src/main/kotlin/com/khureen/greenReview/service/ProductService.kt
+++ src/main/kotlin/com/khureen/greenReview/service/ProductService.kt
@@ -36,7 +36,8 @@ fun Product.toGetDTOWith(score: Optional<ProductScore>): GetProductDTO {
             picUrl = this.picUrl.map { it }.toList(),
             registeredDate = this.registeredDate,
             thumbnailUrl = this.thumbnailUrl,
-            originalUrl = this.originalUrl
+            originalUrl = this.originalUrl,
+            detailpicUrl = this.detailpicUrl
         ),
         score
     )
@@ -58,7 +59,8 @@ class AddProductService {
                 deliveryFee = product.product.deliveryFee,
                 registeredDate = product.product.registeredDate,
                 reviews = mutableListOf(),
-                originalUrl = product.product.originalUrl
+                originalUrl = product.product.originalUrl,
+                detailpicUrl = product.product.detailpicUrl.toMutableList()
             )
         )
     }
