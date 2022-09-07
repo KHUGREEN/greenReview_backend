d97a4a2df1b6e2b169760fd6126f72bfd0eaf661
2480258 <swh8059@naver.com>
2022-09-07 22:40:34 +0900

    change checklist ids

--- src/main/kotlin/com/khureen/greenReview/service/ChecklistService.kt
+++ src/main/kotlin/com/khureen/greenReview/service/ChecklistService.kt
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Service
 @Service
 class ChecklistService {
     companion object {
-        val notSufficientEvidence = ChecklistElement(2, "증거 불충분")
+        val notSufficientEvidence = ChecklistElement(1, "증거 불충분")
 
-        val ambiguousStatement = ChecklistElement(3, "애매모호한 주장")
+        val ambiguousStatement = ChecklistElement(2, "애매모호한 주장")
 
-        val lieStatement = ChecklistElement(5, "거짓말")
+        val lieStatement = ChecklistElement(3, "거짓말")
 
-        val inappropriateCertification = ChecklistElement(7, "부적절한 인증 라벨")
+        val inappropriateCertification = ChecklistElement(4, "부적절한 인증 라벨")
 
         val all = listOf(notSufficientEvidence, ambiguousStatement, lieStatement, inappropriateCertification)
     }
